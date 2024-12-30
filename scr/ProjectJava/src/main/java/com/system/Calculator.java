cfc86df5a03d1f7a8bd0e11613a62cb296ae434b
tanlocnguyen151203 <tanlocnguyen12003@gmail.com>
2024-12-30 18:46:28 +0700

    commit

---
+++ scr/ProjectJava/src/main/java/com/system/Calculator.java
@@ -0,0 +1,22 @@
+package com.system;
+
+public class Calculator {
+    public int tong(int a, int b) {
+        return a + b;
+    }
+
+    public int tru(int a, int b) {
+        return a - b;
+    }
+
+    public int tich(int a, int b) {
+        return a * b;
+    }
+
+    public int thuong(int a, int b) {
+        if (b != 0)
+            return a / b;
+        else
+            throw new IllegalArgumentException("Số chia không được là số 0");
+    }
+}
