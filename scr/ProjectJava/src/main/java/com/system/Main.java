ad3f2625141c16a70a1f39731cf7dd0123904ca2
tanlocnguyen151203 <tanlocnguyen12003@gmail.com>
2025-01-06 20:17:22 +0700

    commit

--- scr/ProjectJava/src/main/java/com/system/Main.java
+++
@@ -1,8 +0,0 @@
-package com.system;
-
-public class Main {
-    public static void main(String[] args) {
-        Calculator c = new Calculator();
-        System.out.println("Hello world! " + c.tong(1, 2));
-    }
-}
